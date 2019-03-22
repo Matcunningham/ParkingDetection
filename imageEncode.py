214a817c758fac3da369d000d465d9381d183162
Miguel <miguel.angel.castorena@gmail.com>
2019-03-21 18:17:46 -0700

    Update imageEncode.py

--- imageEncode.py
+++ imageEncode.py
@@ -1,14 +1,20 @@
 import base64
 from requests import post
-ServerURL = "http://54.186.186.248:3000/api/overlay"
+ServerURL = "http://54.186.186.248:3000/api/overlayimage"
+img = "smiley.jpg"
 
 def postOverlay(filepath, parkinglotID):
     with open(filepath, "rb") as imageFile:
-        fileStr = base64.b64encode(imageFile.read())        #opens file and reads as string, converts to b64 encoding
-    asciiImg = fileStr.decode('ascii')
+        fileBytes = base64.b64encode(imageFile.read())        #opens file and reads as string
+    fileStr = fileBytes.decode('UTF-8')
     print("Sending overlay data...")
-    body = {'parkinglot_id': parkinglotID, 'overlay': asciiImg}
+    body = {'parkinglot_ID': parkinglotID, 'data': fileStr}
     try:
+        print(type(fileStr))
+        print(fileStr)
         msgResponse = post(ServerURL, data=body)  # Post overlay
     except:
         raise ValueError('Status could not be posted')  # If post message fails catch error and print message
+
+
+postOverlay(img, 491)
